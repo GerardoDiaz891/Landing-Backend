f630ec24b7ddff969f20bcf2806bd5a27bbbf7e3
WilliansAS <anthowill1608@gmail.com>
2025-06-23 20:57:09 -0500

    Se implementó la validación del token generado por el reCAPTCHA

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,29 +1,62 @@
-const db = require('../config/db');
+const db = require("../config/db");
+const axios = require("axios");
 
 //CREAR CONTACTOS
-exports.createContact = (req, res) => {
-  const { name, email, phone, message } = req.body;
+exports.createContact = async (req, res) => {
+  const { name, email, phone, message, token } = req.body;
 
-  if (!name || !email || !phone || !message) {
-    return res.status(400).json({ error: 'Todos los campos son requeridos' });
+  console.log("BODY:", req.body);
+
+  if (!name || !email || !phone || !message || !token) {
+    return res.status(400).json({ error: "Todos los campos son requeridos" });
   }
 
-  const sql = 'INSERT INTO contacts (name, email, phone, message) VALUES (?, ?, ?, ?)';
-  db.query(sql, [name, email, phone, message], (err, result) => {
-    if (err) {
-      console.error('Error al insertar:', err);
-      return res.status(500).json({ error: 'Error en el servidor' });
+  try {
+    // Validar el token con Google
+    const secretKey = "6LcUZmsrAAAAAKqbQe2LTtcTSsP88zjMxz5qBiOH";
+    const response = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      null,
+      {
+        params: {
+          secret: secretKey,
+          response: token,
+        },
+      }
+    );
+
+    const data = response.data;
+
+    if (!data.success) {
+      return res.status(403).json({ error: "Verificación reCAPTCHA fallida" });
     }
-    res.status(201).json({ message: 'Contacto guardado correctamente', id: result.insertId });
-  });
+
+    // Si pasó la verificación, guardamos en la base de datos
+    const sql =
+      "INSERT INTO contacts (name, email, phone, message) VALUES (?, ?, ?, ?)";
+    db.query(sql, [name, email, phone, message], (err, result) => {
+      if (err) {
+        console.error("Error al insertar:", err);
+        return res.status(500).json({ error: "Error en el servidor" });
+      }
+
+      res.status(201).json({
+        message: "Contacto guardado correctamente",
+        id: result.insertId,
+      });
+    });
+  } catch (error) {
+    console.error("Error al verificar reCAPTCHA:", error);
+    res.status(500).json({ error: "Error al verificar reCAPTCHA" });
+  }
 };
 
 //OBTENER CONTACTOS
 exports.getContacts = (req, res) => {
-  db.query('SELECT * FROM contacts', (err, results) => {
+  db.query("SELECT * FROM contacts", (err, results) => {
     if (err) {
-      console.error('Error al obtener contactos:', err);
-      return res.status(500).json({ error: 'Error en el servidor' });
+      console.error("Error al obtener contactos:", err);
+      return res.status(500).json({ error: "Error en el servidor" });
     }
     res.json(results);
   });
