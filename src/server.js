195ebc716b2c98386df42a3abe18c0577ab2be69
Gerardo <VampyrTempestmail.com>
2025-07-04 10:51:21 -0500

    ifx-01: Modificación para puerto de Railways

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
